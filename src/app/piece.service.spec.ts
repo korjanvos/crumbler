3a078bde2177bca338669be750e4b8a657304cdf
Raymond Loman <raymond@carpago.nl>
2019-06-17 15:28:13 +0200

    Add test for PieceService::get

--- src/app/piece.service.spec.ts
+++ src/app/piece.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PieceStoreService } from './piece-store.service';
+import { Piece } from './PieceModel';
 
 describe('PieceStoreService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -8,5 +9,9 @@ describe('PieceStoreService', () => {
   it('should be created', () => {
     const service: PieceStoreService = TestBed.get(PieceStoreService);
     expect(service).toBeTruthy();
+    let lastIndex  = service.getPieces().length-1;
+    let p:Piece = new Piece();
+    service.addPiece(p);
+    expect(service.getPieces[lastIndex+1] ===  p);
   });
 });
